af8ef12d2a8a5fa153d7b13def463fcad3fb171a
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-11-01 20:07:03 +0800

    Update compiler.js

--- src/compiler.js
+++ src/compiler.js
@@ -7,8 +7,11 @@ export function transpile(input) {
     
     .replace(/(?<!['"`])\b(log to console)\b(?!['"`])/g,'console.log')
     .replace(/(?<!['"`])\b(otherwise)\b(?!['"`])/g,'else')
+    .replace(/(?<!['"`])\b(try to execute code)\b(?!['"`])/g,'try')
+    .replace(/(?<!['"`])\b(if the code errors)\b(?!['"`])/g,'catch')
     .replace( /(?<!['"`])\b(is equal to)\b(?!['"`])/g,'==')
     .replace(/(?<!['"`])\b(is not equal to)\b(?!['"`])/g,'!=')
+    .replace(/(?<!['"`])\b(is strictly equal to)\b(?!['"`])/g,'===')
     .replace(/(?<!['"`])\b(.increment)\b(?!['"`])/g,'++')
     .replace(/(?<!['"`])\b(.decrement)\b(?!['"`])/g,'--')
     .replace(/(?<!['"`])\b(classificate class named)\b(?!['"`])/g,'class')
@@ -17,7 +20,9 @@ export function transpile(input) {
     .replace(/(?<!['"`])\b(respond with)\b(?!['"`])/g,'return')
     .replace(/(?<!['"`])\b(forward)\b(?!['"`])/g,'import')
     .replace(/(?<!['"`])\b(directly from)\b(?!['"`])/g,'from')
+    .replace(/(?<!['"])\b(£)\b(?!['"])/g,'$')
     .replace(/(?<!['"`])\b(~)\b(?!['"`])/g,'.')
+    .replace(/(?<!['"`])\b(awaitively execute)\b(?!['"`])/g,'await')
   
   return output;
 }
